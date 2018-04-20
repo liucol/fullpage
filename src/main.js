a4e206b523c96a94913ac95a3686c3cd8ec3a2cd
liucol <pearl-iooi@foxmail.com>
2018-04-20 17:28:10 +0800

    上下滚动的vue设置

--- src/main.js
+++ src/main.js
@@ -10,12 +10,23 @@ new Vue({
   el: '#app',
   data:{
     bgColor: ["#c09","#90c","#09c","#9c0"],
-    curindex: 0
+    curindex: 0,
+    name: "down"
   },
   methods:{
     wheelEvent(e){
         if(e.deltaY > 0){
-           this.curindex++
+          if(this.curindex === this.bgColor.length-1){
+               return
+          }
+          this.name = "down"
+           this.curindex++;
+        }else{
+          if(this.curindex === 0){
+            return
+          }
+          this.name = "up"
+          this.curindex--;
         }
     }
   }
