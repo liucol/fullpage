9eaeadd415fab893ac2af85b7b620404e09f27e9
liucol <pearl-iooi@foxmail.com>
2018-04-20 10:39:32 +0800

    fullpage的开始

---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+//import App from './App'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  data:{
+    bgColor: ["#c09","#90c","#09c","#9c0"],
+    curindex: 0
+  },
+  methods:{
+    wheelEvent(e){
+        if(e.deltaY > 0){
+           this.curindex++
+        }
+    }
+  }
+  // components: { App },
+  // template: '<App/>'
+})
